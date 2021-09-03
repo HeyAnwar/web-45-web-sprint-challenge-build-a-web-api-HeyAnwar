2bf387a00bda2c71b34bf54adc645c9de98bc33f
Anwar <AnwarAldogmi@gmail.com>
2021-09-03 14:17:00 -0700

    MVP (1 failing)

--- api/projects/projects-middleware.js
+++ api/projects/projects-middleware.js
@@ -1,1 +1,31 @@
 // add middlewares here related to projects
+async function validateProject(req, res, next) {
+    try {
+        const  project  = req.params.id
+        if (!project) {
+        res.status(400).json({
+            message: 'not a valid project'
+        })
+    } else {
+        next()
+    }
+    } catch (err) {
+        next(err)
+    }
+}
+
+// function validateProject(req, res, next) {
+//     const { project } = req.body
+//     if(!project || !project.trim()) {
+//       res.status(400).json({
+//         message: 'not a valid project'
+//       })
+//     } else {
+//       req.project = project.trim()
+//       next()
+//     }
+//   }
+
+module.exports = {
+    validateProject
+}
